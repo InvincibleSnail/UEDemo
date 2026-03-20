690ed852eea53d61bb4b2f5ba461aa2bdf6c27ed
User <user@UserdeMacBook-Pro-3.local>
2026-03-20 11:46:04 +0800

    rm logic

--- Source/UEDemo/UEDemo.Build.cs
+++ Source/UEDemo/UEDemo.Build.cs
@@ -14,31 +14,11 @@ public class UEDemo : ModuleRules
 			"Engine",
 			"InputCore",
 			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
 			"UMG",
 			"Slate"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
-
-		PublicIncludePaths.AddRange(new string[] {
-			"UEDemo",
-			"UEDemo/Variant_Platforming",
-			"UEDemo/Variant_Platforming/Animation",
-			"UEDemo/Variant_Combat",
-			"UEDemo/Variant_Combat/AI",
-			"UEDemo/Variant_Combat/Animation",
-			"UEDemo/Variant_Combat/Gameplay",
-			"UEDemo/Variant_Combat/Interfaces",
-			"UEDemo/Variant_Combat/UI",
-			"UEDemo/Variant_SideScrolling",
-			"UEDemo/Variant_SideScrolling/AI",
-			"UEDemo/Variant_SideScrolling/Gameplay",
-			"UEDemo/Variant_SideScrolling/Interfaces",
-			"UEDemo/Variant_SideScrolling/UI"
-		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
